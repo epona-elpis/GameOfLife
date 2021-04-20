d9311d3b31bf8300cf6c9e97ebd0221ce63d28b9
epona-elpis <kim@jkraft.co.uk>
2021-04-20 11:07:17 +0100

    refactored

--- src/main/java/game/AliveRule.java
+++ src/main/java/game/AliveRule.java
@@ -2,12 +2,12 @@ package game;
 
 class AliveRule {
     private final static int OVER_POPULATED = 4;
-    private final static int MINIMUM_NEIGHBOURS = 2;
+    private final static int UNDER_POULATED = 1;
     private final static int NEIGHBOURS_REQUIRED_FOR_DEAD_CELL_TO_BE_BORN = 3;
 
     boolean isAlive(boolean currentAliveState, int neighbours){
         if (currentAliveState ){
-            return neighbours < OVER_POPULATED && neighbours >= MINIMUM_NEIGHBOURS;
+            return neighbours < OVER_POPULATED && neighbours > UNDER_POULATED;
         }
         return neighbours == NEIGHBOURS_REQUIRED_FOR_DEAD_CELL_TO_BE_BORN;
     }
